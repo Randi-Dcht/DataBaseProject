0fc37ee695377cd0b0903c27eb873cdd7522f8b3
190134 <190134@umons.ac.be>
2019-11-12 08:32:34 +0100

    add the class with method structure

---
+++ Depedance.java
@@ -0,0 +1,24 @@
+package be.ac.umons.SGBD;
+public class Dependance
+{
+  public static void main(String[] args)
+  {
+    
+  }
+  public void FuncDept(String table,String lhs, String rhs)
+  {
+
+  }
+  public boolean BCNF(String Table)
+  {
+
+  }
+  public boolean 3NF(String Table)
+  {
+
+  }
+  public String[] Decomposition(String Table)
+  {
+
+  }
+}
