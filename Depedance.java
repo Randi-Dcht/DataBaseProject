028f62c440b8c9754178f261586606c88bbc14d7
190134 <190134@umons.ac.be>
2019-11-15 12:11:18 +0100

    changed the methods and add the multiple lhs

--- Depedance.java
+++ Depedance.java
@@ -1,24 +1,31 @@
 package be.ac.umons.SGBD;
+import java.sql.*;
+
 public class Dependance
 {
   public static void main(String[] args)
   {
-    
-  }
-  public void FuncDept(String table,String lhs, String rhs)
-  {
 
   }
+
+  public void funcDept(String table,String lhs, String rhs)
+  {
+    String[] lhs2 = lhs.split(" ");
+  }
+
   public boolean BCNF(String Table)
   {
-
   }
+
   public boolean 3NF(String Table)
   {
-
   }
-  public String[] Decomposition(String Table)
-  {
 
+  public void addDependency()
+  {
+  }
+
+  public String[] removeDependency()
+  {
   }
 }
