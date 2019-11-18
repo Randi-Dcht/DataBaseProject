e2a51e3cf27f0a06b4f986b63f58f8c7cb2aa244
190134 <190134@umons.ac.be>
2019-11-18 20:05:10 +0100

    method to use sqLite

--- BaseDonne.java
+++ BaseDonne.java
@@ -15,7 +15,9 @@ public class BaseDonne
   {
     BaseDonne bd = new BaseDonne("jdbc:sqlite:");
     bd.connection("TestRandy.db");
-    bd.creerBaseDonnee();
+    //bd.creerBaseDonnee();
+    //bd.creerTable("CREATE TABLE etudiant(Matricule integer PRIMARY KEY, Nom text, Faculte text)");
+    bd.insererTable("INSERT INTO etudiant(Matricule,Nom,Faculte) VALUES(1234,'rnd','sciences')");
     bd.terminer();
   }
   /*-----------------------------Test de cette classe------------------------<*/
@@ -32,7 +34,7 @@ public class BaseDonne
   {
     try
     {
-      Class.forName("org.sqlite.JDBC");
+      Class.forName("org.sqlite.JDBC"); /*<= permet de dire le fichier jar*/
       connection = DriverManager.getConnection(url + fichier);
       /*Supprimer =>*/ System.out.println("Connection est bien réussite avec la Data Base");
       return true;
@@ -57,6 +59,38 @@ public class BaseDonne
     catch(Exception e)
     {
       /*Supprimer =>*/ System.out.println("Erreur lors de la création de la base de donnée !!");
+      return false;
+    }
+  }
+
+  public boolean creerTable(String createTable)
+  {
+    try
+    {
+      Statement statement = connection.createStatement();
+      statement.execute(createTable);
+      /*Supprimer =>*/ System.out.println("La table a bien été créée dans la base de données");
+      return true;
+    }
+    catch(Exception e)
+    {
+      /*Supprimer =>*/ System.out.println("Oups une erreur lors de la création table");
+      return false;
+    }
+  }
+
+  public boolean insererTable(String quoi)
+  {
+    try
+    {
+      PreparedStatement precmd = connection.prepareStatement(quoi);
+      precmd.executeUpdate();
+      /*Supprimer =>*/System.out.println("Insertion dans la table est correctement placée :-)");
+      return true;
+    }
+    catch(Exception e)
+    {
+      /*Supprimer =>*/System.out.println("Erreur lors de l'insertion dans la table : " + e);
       return false;
     }
   }
