68c326b873d2459f4eaefe4614b42a04db479a5a
Guillaume Cardoen <190532@student.umons.ac.be>
2019-12-07 14:00:38 +0100

    Completed command ListDF (can take one argument : the table's name)

--- code/src/be/ac/umons/projetBDD/CommandParser.java
+++ code/src/be/ac/umons/projetBDD/CommandParser.java
@@ -12,26 +12,39 @@ public class CommandParser {
         String[] commTab = comm.split(" ");
         switch (commTab[0].toLowerCase()) {
             case "listdf":
-                listDF();
+                if (commTab.length > 2) {
+                    System.err.println("The command ListDF takes 1 or 2 argument(s) !\nUsage : ListDF [table_name]");
+                    break;
+                }
+                else if (commTab.length == 2)
+                    listDF(commTab[1]);
+                else
+                    listDF();
                 break;
             case "adddf":
                 if (commTab.length != 4) {
-                    System.err.println("The command adddf takes 3 arguments !\nUsage : adddf <tableName> <lhs> <rhs>");
+                    System.err.println("The command AddDF takes 3 arguments !\nUsage : AddDF <tableName> <lhs> <rhs>");
                     break;
                 }
                 addDF(commTab[1], commTab[2], commTab[3]);
                 break;
             default:
                 System.err.println(String.format("Command \"%s\" isn't defined !", commTab[0]));
-
         }
     }
 
     private void listDF() {
-        System.out.println("DF in DB : (<tableName> : <lhs> -> <rhs>)");
         for (String key : db.getDependenciesMap().keySet())
-            for (Dependance dep: db.getDependenciesMap().get(key))
-                System.out.println(String.format("  %s : %s -> %s", dep.getTableName(), dep.getLhs(), dep.getRhs()));
+            listDF(key);
+    }
+
+    private void listDF(String tableName) {
+        System.out.println(String.format("DF in %s : (<tableName> : <lhs> -> <rhs>)", tableName));
+        if (! db.getDependenciesMap().containsKey(tableName))
+            System.err.println(String.format("The table %s doesn't exist !", tableName));
+        for (Dependance dep: db.getDependenciesMap().get(tableName))
+            System.out.println(String.format("  %s : %s -> %s", dep.getTableName(), dep.getLhs(), dep.getRhs()));
+
     }
 
     private void addDF(String tableName, String lhs, String rhs) {
