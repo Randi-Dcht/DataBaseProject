c89fe047bd046ba4fe26705ee4e102545b2502fa
190134 <190134@umons.ac.be>
2019-12-17 15:21:43 +0100

    add the comments to redandunce

--- code/src/be/ac/umons/projetBDD/Commands/RemoveRedundantDependencies.java
+++ code/src/be/ac/umons/projetBDD/Commands/RemoveRedundantDependencies.java
@@ -3,16 +3,31 @@ package be.ac.umons.projetBDD.Commands;
 import be.ac.umons.projetBDD.Dependence;
 import be.ac.umons.projetBDD.Main;
 import be.ac.umons.projetBDD.Sql;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 
-import java.util.*;
-
+/**
+ * This class allows to remove a dependence in the dataBase
+ * @author Guillaume Cardoen (étudiants en sciences informatique Umons)
+ */
 public class RemoveRedundantDependencies extends CommandDF {
+
+    /**
+     * This constructor allows to give a dataBAse and the arguments of this function
+     * @param db who is the dataBase (sql)
+     * @param  args who are the arguments of this command
+     */
     public RemoveRedundantDependencies(Sql db, String[] args) {
         super(db, args);
         possibleNumberOfArgs.add(1);
         possibleNumberOfArgs.add(2);
     }
 
+    /**
+     * This method allows to launch the remove redundant dependence in the dataBase
+     */
     @Override
     protected void doAction() {
         List<Dependence> dependenceList = db.getDependenciesMap().get(args[1]);
@@ -37,6 +52,11 @@ public class RemoveRedundantDependencies extends CommandDF {
         }
     }
 
+    /**
+     * This method allows to check the redundancy dependence of this dataBase
+     * @param dep who is the dependence for check if redundancies
+     * @return true if the dependence true if exist and false otherwise
+     */
     public boolean isRedundant(Dependence dep) {
         List<Dependence> dependenceList = db.getDependenciesMap().get(args[1]);
         if (dep.getLhs().contains(dep.getRhs())) {
@@ -61,12 +81,21 @@ public class RemoveRedundantDependencies extends CommandDF {
         return foundOne.contains(dep.getRhs());
     }
 
+    /**
+     * This method allows to ask to user if he want to delete dependence in the dataBase
+     * @param oneToDel who is the dependence to remove
+     * @return true if the user choose yes and false otherwise
+     */
     private boolean askForDeleting(Dependence oneToDel) {
         System.out.println(String.format("Are you sure you want to delete %s -> %s from the table (%s) ? (y/N)", oneToDel.getLhs(), oneToDel.getRhs(), args[1]));
         String ans = Main.input.nextLine().trim().toLowerCase();
         return ans.equals("y");
     }
 
+    /**
+     * This method allows to give the function of this command
+     * @return function of this command
+     */
     @Override
     public String getUsage() {
         return "(RemoveLogicalDependencies | rdd) <table_name>";
