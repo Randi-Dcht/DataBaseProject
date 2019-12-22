4ce7fc9aaabf27d32d44a015664d9a7c6ae8e4cf
190134 <190134@umons.ac.be>
2019-12-22 18:21:17 +0100

    add main of GUI

--- code/GUI/be/ac/umons/projetBDD/GUI/MainGui.java
+++ code/GUI/be/ac/umons/projetBDD/GUI/MainGui.java
@@ -1,11 +1,20 @@
 package be.ac.umons.projetBDD.GUI;
 
+import be.ac.umons.projetBDD.Saving;
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
+
+import java.io.File;
+import java.util.ArrayList;
 
 public class MainGui extends Application
 {
@@ -14,36 +23,133 @@ public class MainGui extends Application
         launch(args);
     }
 
+    public static final int widthX  = 1000;
+    public static final int heightY = 900;
+    public static final String css = "../../../../../../misc/ressource/designMain.css";
+
+    /*Les variables de l'application*/
+    private Stage stage;
+    private HBox top;
+    private VBox entry;
+    private BorderPane fondEcran;
+    private String roadNew ="misc/";
+
     @Override
     public void start(Stage primaryStage)
     {
-        BorderPane pane = new BorderPane();
-        Scene scene = new Scene(pane,1000,1000);
+        fondEcran = new BorderPane();
+        Scene scene = new Scene(fondEcran,widthX,heightY);
+        scene.getStylesheets().add(getClass().getResource(css).toExternalForm());
+        fondEcran.getStyleClass().add("fondEcran");
 
-        pane.setCenter(createChoose());
+        entry = choiseEntry();
+        entry.setAlignment(Pos.CENTER);
+        fondEcran.setCenter(entry);
 
-        Button bt = new Button("ok");
-        bt.setOnAction(e->{System.out.println("ok");});
-
-        pane.setBottom(bt);
-
+        primaryStage.setTitle("Main of SGBD");
         primaryStage.setScene(scene);
-        primaryStage.setTitle("MainGui SGBD");
+        //primaryStage.setOnCloseRequest(Event::consume);
         primaryStage.show();
+        stage = primaryStage;
+
     }
 
-    public VBox createChoose()
+    public VBox choiseEntry()
     {
-        return null;
+        VBox hb = new VBox();
+
+        Button okold = new Button("Find an old DataBase");
+        HBox hb1 = new HBox();
+        hb1.getChildren().addAll(okold);
+        hb1.setSpacing(50);
+        hb1.setAlignment(Pos.CENTER);
+
+        Button oknew = new Button("Created a  DataBase");
+        HBox hb2 = new HBox();
+        hb2.getChildren().add(oknew);
+        hb2.setSpacing(50);
+        hb2.setAlignment(Pos.CENTER);
+
+        Button quit = new Button("quit Application");
+        Button param = new Button("help about this");
+
+        hb.setSpacing(30);
+        hb.getChildren().addAll(hb1,hb2,param,quit);
+
+        oknew.setOnAction(e->{dnew(hb2,oknew);});
+        okold.setOnAction(e->{dold(hb1,okold);});
+        param.setOnAction(e->{System.out.println("76 : paramètre de l'application");});
+        quit.setOnAction(e->{stage.close();});
+
+        return hb;
     }
 
-    public VBox useOld()
+    public void dold(HBox hb,Button bt)
     {
-        return null;
+        ComboBox<String> oldBase = new ComboBox<String>(); oldBase.getStyleClass().add("text");
+        oldBase.getItems().addAll(find(roadNew.toString()));
+        oldBase.setPrefWidth(350);
+        oldBase.setPrefHeight(30);
+        Button rp  = new Button("...");
+        Button ok1 = new Button("ok");
+        hb.getChildren().addAll(oldBase,rp,ok1);
+        hb.getChildren().remove(bt);
+
+        rp.setOnAction(e->{repertory();});
+        ok1.setOnAction(e->{changedOld(roadNew+oldBase.getValue());});
     }
 
-    public VBox useNew()
+    public void dnew(HBox hb,Button bt)
     {
-        return null;
+        TextField texte = new TextField(); texte.getStyleClass().add("text");
+        texte.setText("NameOfDatabase");
+        texte.setPrefWidth(350);
+        texte.setPrefHeight(30);
+        Button rp  = new Button("...");
+        Button ok2 = new Button("ok");
+        hb.getChildren().addAll(texte,rp,ok2);
+        hb.getChildren().remove(bt);
+
+        rp.setOnAction(e->{repertory();});
+        ok2.setOnAction(e->{changedNew(roadNew+texte.getText());});
     }
+
+    public void changedOld(String name)
+    {
+
+        stage.close();
+    }
+
+    public void changedNew(String name)
+    {
+        new NewDataBase(name).start(new Stage());
+        stage.close();
+    }
+
+    public ArrayList<String> find(String rd)
+    {
+        File repert = new File(rd);
+        String[] lst = repert.list();
+        ArrayList<String> lt = new ArrayList<String>();
+        assert lst != null;
+        for(String str : lst)
+        {
+            String[] cut = str.split("[/.]");
+            if(cut.length ==2 && cut[1].equals("sgbd"))
+                lt.add(cut[0]);
+        }
+        return lt;
+    }
+
+    public void repertory()
+    {
+        DirectoryChooser rep = new DirectoryChooser();
+        File file = rep.showDialog(stage);
+        System.out.println("151: " + file.toString());
+        if(file != null) {
+            roadNew = file.toString() + "/";
+            System.out.println("ok");
+        }
+    }
+
 }
---
+++ code/GUI/be/ac/umons/projetBDD/GUI/NewDataBase.java
@@ -0,0 +1,133 @@
+package be.ac.umons.projetBDD.GUI;
+
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class NewDataBase extends Application
+{
+    private Stage stage;
+    private Button add;
+    private VBox center;
+    private TextField title;
+    private HashMap<String, ArrayList<String>> list;
+    private String dat;
+    static final String dep = " ... ";
+    static final String tab = " Name of this table ";
+
+    public NewDataBase(String dataBAse)
+    {
+        dat = dataBAse;
+    }
+
+    @Override
+    public void start(Stage stage)
+    {
+        BorderPane pane = new BorderPane();
+        Scene scene = new Scene(pane,1500,1000);
+
+        center = new VBox();
+        center.setSpacing(5);
+        addTable();
+        title = new TextField(dat);
+        pane.setTop(title);
+        pane.setBottom(underPage());
+        pane.setCenter(center);
+
+        stage.setTitle("Create a new DataBase");
+        stage.setScene(scene);
+        stage.show();
+
+        list = new HashMap<>();
+        this.stage = stage;
+    }
+
+    public HBox underPage()
+    {
+        HBox hb = new HBox();
+        hb.setAlignment(Pos.BOTTOM_CENTER);
+        hb.setSpacing(40);
+        add = new Button("add Table");
+        Button end = new Button("next");
+        end.setOnAction(e->
+        {
+            finished();
+        });
+        add.setOnAction(e->
+        {
+            addTable();
+        });
+        hb.getChildren().addAll(add,end);
+        return hb;
+    }
+
+    public void addTable()
+    {
+        VBox vvb = new VBox();
+        vvb.setSpacing(5);
+        TextField txt = new TextField(tab);
+        Button more = new Button("add attribute");
+        more.setOnAction(e->
+        {
+            String what = txt.getText();
+            if(!what.equals(tab))
+            {
+                add(what);
+                vvb.getChildren().add(addAttribute(more,what));
+            }
+        });
+        vvb.getChildren().addAll(txt,more);
+        center.getChildren().addAll(vvb);
+    }
+
+    private void add(String name)
+    {
+        if(!list.containsKey(name))
+            list.put(name,new ArrayList<>());
+    }
+
+    private void add(String name, String lt)
+    {
+        if(list.containsKey(name))
+            list.get(name).add(lt);
+    }
+
+    public HBox addAttribute(Button bt,String tab)
+    {
+        HBox hb = new HBox();
+        hb.setSpacing(20);
+        bt.setVisible(false);
+        TextField att = new TextField(dep);
+        Button ok = new Button("Ok");
+        ok.setOnAction(e->
+        {
+            String what = att.getText();
+            if(!what.equals(dep))
+            {
+                bt.setVisible(true);
+                ok.setVisible(false);
+                add(tab,what);
+            }
+        });
+        hb.getChildren().addAll(new Text("Attribute : "),att,ok);
+        return hb;
+    }
+
+    public void finished()
+    {
+        if(list.size() != 0)
+            stage.close();
+        for(String str : list.keySet())
+            System.out.println(str + " -> " + list.get(str));
+
+    }
+}
